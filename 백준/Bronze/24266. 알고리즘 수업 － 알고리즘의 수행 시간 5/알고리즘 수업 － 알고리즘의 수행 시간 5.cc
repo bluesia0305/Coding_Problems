c964a6bf928c96ef7ec65b976e075b9c68c213aa
ssumdaeng_e <79248724+bluesia0305@users.noreply.github.com>
2023-05-28 14:17:51 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	unsigned long long N;
+	cin >> N;
+	cout << N * N * N << "\n" << 3 << "\n";
+
+	return 0;
+}
