5ea322c5815cf2c05e99f1fcb226b5545412b6d4
ssumdaeng_e <79248724+bluesia0305@users.noreply.github.com>
2023-05-28 14:13:23 +0900

    [Silver II] Title: 연속합, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n, k, max, memoize;
+	cin >> n >> k;
+	max = memoize = k;
+	n--;
+
+	while (n--)
+	{
+		cin >> k;
+		if (max < k)
+			max = k;
+
+		if (memoize + k > 0)
+		{
+			memoize += k;
+			if (max < memoize)
+				max = memoize;
+		}
+		else
+			memoize = 0;
+	}
+	
+	cout << max << "\n";
+
+	return 0;
+}
