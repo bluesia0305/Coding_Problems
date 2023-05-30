177b220600d12af4b774e29e01902dfd5679cf1c
ssumdaeng_e <79248724+bluesia0305@users.noreply.github.com>
2023-05-30 21:54:03 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 40 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -12,32 +12,21 @@ int main()
 	int N, M;
 	cin >> N >> M;
 
-	vector<int> Nums(N);
 	vector<int> FwdTab(N);
-	vector<int> BwdTab(N);
 	for (int i = 0; i < N; i++)
-		cin >> Nums[i];
-
-	FwdTab[0] = Nums[0];
-	BwdTab[N - 1] = Nums[N - 1];
-	for (int i = 1; i < N; i++)
 	{
-		FwdTab[i] = FwdTab[i - 1] + Nums[i];
-		BwdTab[N - 1 - i] = BwdTab[N - i] + Nums[N - 1 - i];
+		int num;
+		cin >> num;
+
+		FwdTab[i] = (i ? FwdTab[i - 1] : 0) + num;
 	}
 	
 	while (M--)
 	{
 		int i, j;
 		cin >> i >> j;
-	
-		int Sum = FwdTab[N - 1];
-		if (i > 1)
-			Sum -= FwdTab[i - 2];
-		if (j < N)
-			Sum -= BwdTab[j];
-	
-		cout << Sum << "\n";
+
+		cout << FwdTab[j - 1] - (i > 1 ? FwdTab[i - 2] : 0) << "\n";
 	}
 	
 	return 0;
