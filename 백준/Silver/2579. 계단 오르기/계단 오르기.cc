fb5f5c28789035728a72cf60d073007d6bcedf50
ssumdaeng_e <79248724+bluesia0305@users.noreply.github.com>
2023-06-10 01:20:00 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -3,7 +3,6 @@
 using namespace std;
 
 int Stairs[301];
-int Scores[301];
 
 int main()
 {
@@ -14,20 +13,26 @@ int main()
 	int N;
 	cin >> N;
 
+	int dp[4] = { 0, };
 	for (int i = 1; i <= N; i++)
 	{
 		cin >> Stairs[i];
 
 		if (i > 2)
 		{
-			int score = Stairs[i - 1] + Scores[i - 3];
-			Scores[i] = Stairs[i] + (Scores[i - 2] > score ? Scores[i - 2] : score);
+			int score = Stairs[i - 1] + dp[0];
+			dp[3] = Stairs[i] + (dp[1] > score ? dp[1] : score);
+
+			dp[0] = dp[1];
+			dp[1] = dp[2];
+			dp[2] = dp[3];
 		}
 		else if (i == 1)
-			Scores[i] = Stairs[i];
+			dp[1] = Stairs[1];
 		else if (i == 2)
-			Scores[i] = Stairs[i] + Stairs[i - 1];
+			dp[2] = Stairs[1] + Stairs[2];
 	}
-	cout << Scores[N] << "\n";
+
+	cout << (N > 2 ? dp[3] : dp[N]) << "\n";
 	return 0;
 }
