6b4802df1a0c31fb5043655c3d3b8e0bc64d580f
ssumdaeng_e <79248724+bluesia0305@users.noreply.github.com>
2023-06-11 02:29:00 +0900

    [Silver V] Title: 단어 정렬, Time: 44 ms, Memory: 3976 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class cmp
+{
+public:
+	bool operator()(string a, string b) const
+	{
+		if (a.length() == b.length())
+			return a < b;
+		
+		return a.length() < b.length();
+	}
+};
+
+string Strings[20000];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+		cin >> Strings[i];
+
+	sort(&Strings[0], &Strings[N - 1] + 1,
+		 [](string a, string b)
+		 {
+			if (a.length() == b.length())
+				return a < b;
+		 	return a.length() < b.length();
+		 });
+
+	string temp;
+	for (int i = 0; i < N; i++)
+	{
+		if (Strings[i] != temp)
+		{
+			temp = Strings[i];
+			cout << temp << "\n";
+		}
+	}
+
+	return 0;
+}
