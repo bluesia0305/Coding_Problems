a3cf8904265b09f7f2284beb87abf68a77ff7b2e
ssumdaeng_e <79248724+bluesia0305@users.noreply.github.com>
2023-06-14 18:38:05 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -1,79 +1,31 @@
 #include <iostream>
+#include <queue>
 
 using namespace std;
-
-class CQueue
-{
-public:
-	CQueue(int N)
-	{
-		Numbers = new int[N + 1];
-		Capacity = Rear = N;
-		for (int i = 0; i < N + 1; i++)
-			Numbers[i] = i;
-	}
-
-	~CQueue()
-	{
-		delete[] Numbers;
-	}
-
-	void Enqueue(int n)
-	{
-		if (IsFull())
-			return;
-
-		Rear = ++Rear % (Capacity + 1);
-		Numbers[Rear] = n;
-	}
-
-	int Dequeue()
-	{
-		if (IsEmpty())
-			return -1;
-
-		Front = ++Front % (Capacity + 1);
-		int temp = Numbers[Front];
-		Numbers[Front] = 0;
-		return temp;
-	}
-
-	bool IsFull() const
-	{
-		return Front == (Rear + 1) % (Capacity + 1);
-	}
-
-	bool IsEmpty() const
-	{
-		return Front == Rear;
-	}
-
-private:
-	int* Numbers;
-	int Capacity;
-	int Front = 0;
-	int Rear = 1;
-};
 
 int main()
 {
 	int N, K;
 	cin >> N >> K;
 
-	CQueue cq = CQueue(N);
+	queue<int> q;
+	for (int i = 1; i <= N; i++)
+		q.push(i);
 
 	cout << "<";
 
 	int i = 1;
 	while (true)
 	{
-		int num = cq.Dequeue();
+		int num = q.front();
+		q.pop();
+
 		if (i++ % K)
-			cq.Enqueue(num);
+			q.push(num);
 		else
 		{
 			cout << num;
-			if (cq.IsEmpty())
+			if (q.empty())
 				break;
 
 			cout << ", ";
