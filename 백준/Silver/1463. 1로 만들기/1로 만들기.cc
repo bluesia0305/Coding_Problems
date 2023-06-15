cd3c888ca1deb7d5b2e9d9a28698f909c4fac76e
ssumdaeng_e <79248724+bluesia0305@users.noreply.github.com>
2023-06-15 15:26:23 +0900

    [Silver III] Title: 1로 만들기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -3,27 +3,15 @@
 
 using namespace std;
 
-char Counts[1000001];
+int Counts(int n)
+{
+	return n <= 1 ? 0 : min(Counts(n / 2) + n % 2, Counts(n / 3) + n % 3) + 1;
+}
 
 int main()
 {
 	int N;
 	cin >> N;
-
-	int i = 0;
-	while (i++ < N)
-	{
-		char cnt = Counts[i] + 1;
-
-		if (3 * i <= N)
-			Counts[3 * i] = Counts[3 * i] ? min(Counts[3 * i], cnt) : cnt;
-
-		if (2 * i <= N)
-			Counts[2 * i] = Counts[2 * i] ? min(Counts[2 * i], cnt) : cnt;
-
-		Counts[i + 1] = Counts[i + 1] ? min(Counts[i + 1], cnt) : cnt;
-	}
-
-	cout << (int)Counts[N] << "\n";
+	cout << Counts(N) << "\n";
 	return 0;
 }
