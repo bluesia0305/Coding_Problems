00029b87d764503cf6d9887919373e54999a75f0
ssumdaeng_e <79248724+bluesia0305@users.noreply.github.com>
2023-06-16 19:50:10 +0900

    [Gold IV] Title: N-Queen, Time: 1212 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -2,12 +2,6 @@
 
 using namespace std;
 
-struct Coord
-{
-	int X;
-	int Y;
-};
-
 int N;
 int Count;
 int Placed[15][15];
@@ -16,7 +10,7 @@ void SubordinatePaths(int x, int y, bool bEliminate)
 {
 	int left = x, right = x;
 	int sign = bEliminate ? 1 : -1;
-	for (int i = y + 1; i < N; i++)
+	for (int i = y; i < N; i++)
 	{
 		Placed[i][x] += sign;
 		if (--left >= 0) Placed[i][left] += sign;
@@ -28,14 +22,14 @@ void PlaceQueen(int X, int placedQueens)
 {
 	SubordinatePaths(X, placedQueens, true);
 
-	if (placedQueens == N - 1)
+	if (placedQueens == N)
 	{
 		Count++;
 		return;
 	}
 
 	for (int x = 0; x < N; x++)
-		if (Placed[placedQueens + 1][x] == 0)
+		if (Placed[placedQueens][x] == 0)
 			PlaceQueen(x, placedQueens + 1);
 	
 	SubordinatePaths(X, placedQueens, false);
@@ -46,7 +40,7 @@ int main()
 	cin >> N;
 
 	for (int x = 0; x < N; x++)
-		PlaceQueen(x, 0);
+		PlaceQueen(x, 1);
 
 	cout << Count << "\n";
 	return 0;
