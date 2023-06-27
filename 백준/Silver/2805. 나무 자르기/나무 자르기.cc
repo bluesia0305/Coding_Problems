aeeddb4ed828d32bee2f9e1929eb7b5fa9a67687
ssumdaeng_e <79248724+bluesia0305@users.noreply.github.com>
2023-06-27 14:13:52 +0900

    [Silver II] Title: 나무 자르기, Time: 148 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.cc
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -2,14 +2,15 @@
 
 using namespace std;
 
-int N, M;
-int Woods[1000000];
+int N;
+unsigned long long M;
+unsigned Woods[1000000];
 
 unsigned long long ObtainWoods(unsigned len)
 {
 	unsigned long long meters = 0;
 	for (int i = 0; i < N; i++)
-		if (Woods[i] > (int)len)
+		if (Woods[i] > len)
 			meters += Woods[i] - len;
 
 	return meters;
@@ -17,23 +18,27 @@ unsigned long long ObtainWoods(unsigned len)
 
 int main()
 {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
 	cin >> N >> M;
 
-	unsigned long long minLen = 1;
-	unsigned long long maxLen = 0;
+	unsigned minLen = 1;
+	unsigned maxLen = 0;
 	for (int i = 0; i < N; i++)
 	{
 		cin >> Woods[i];
-		if ((int)maxLen < Woods[i])
+		if (maxLen < Woods[i])
 			maxLen = Woods[i];
 	}
 
 	maxLen++;
 	while (minLen < maxLen)
 	{
-		unsigned long long middle = (minLen + maxLen) / 2;
+		unsigned middle = (minLen + maxLen) / 2;
 
-		if (ObtainWoods(middle) < (unsigned long long)M)
+		if (ObtainWoods(middle) < M)
 			maxLen = middle;
 		else
 			minLen = middle + 1;
