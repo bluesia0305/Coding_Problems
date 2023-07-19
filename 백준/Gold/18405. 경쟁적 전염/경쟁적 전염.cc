7c29cb668367869f66e40b281a40c48bcc355f5b
ssumdaeng_e <79248724+bluesia0305@users.noreply.github.com>
2023-07-19 15:39:54 +0900

    [Gold V] Title: 경쟁적 전염, Time: 16 ms, Memory: 2476 KB -BaekjoonHub

---
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.cc
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int TestTube[200][200];
+int N, K, S, X, Y;
+
+struct Coord
+{
+	int Row;
+	int Col;
+
+	bool operator>(const Coord& Other) const
+	{ return TestTube[Row][Col] > TestTube[Other.Row][Other.Col]; }
+};
+
+priority_queue<Coord, vector<Coord>, greater<>> PQ;
+
+void Initiate()
+{
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			if (TestTube[i][j])
+				PQ.emplace(Coord{ i, j });
+}
+
+void Contaminate(priority_queue<Coord, vector<Coord>, greater<>>& pq, int row, int col, int dRow, int dCol)
+{
+	int x = row + dRow, y = col + dCol;
+	if (x >= 0 && x < N &&
+		y >= 0 && y < N &&
+		!TestTube[x][y])
+	{
+		TestTube[x][y] = TestTube[row][col];
+		pq.emplace(Coord{ x, y });
+	}
+}
+
+void Propagate()
+{
+	if (TestTube[X - 1][Y - 1])
+		return;
+
+	priority_queue<Coord, vector<Coord>, greater<>> pq;
+	while (!PQ.empty())
+	{
+		Coord coord = PQ.top();
+		PQ.pop();
+
+		if (TestTube[coord.Row][coord.Col])
+		{
+			Contaminate(pq, coord.Row, coord.Col, 1, 0);
+			Contaminate(pq, coord.Row, coord.Col, 0, 1);
+			Contaminate(pq, coord.Row, coord.Col, -1, 0);
+			Contaminate(pq, coord.Row, coord.Col, 0, -1);
+		}
+	}
+
+	PQ.swap(pq);
+}
+
+int main()
+{
+	cin >> N >> K;
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			cin >> TestTube[i][j];
+
+	cin >> S >> X >> Y;
+
+	Initiate();
+
+	for (int i = 0; i < S; i++)
+		Propagate();
+
+	cout << TestTube[X - 1][Y - 1] << "\n";
+	return 0;
+}
