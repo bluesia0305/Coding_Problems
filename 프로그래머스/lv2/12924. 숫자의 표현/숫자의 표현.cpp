882b24a01e6b563ad7c576bafc00fc89957bb923
ssumdaeng_e <79248724+bluesia0305@users.noreply.github.com>
2023-08-13 03:14:11 +0900

    [level 2] Title: 숫자의 표현, Time: 0.02 ms, Memory: 3.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n)
+{
+    int answer = 1;
+    for (int i = 1; i <= n / 2; i++)
+    {
+        int sum = 0, j = i;
+        while (sum < n)
+            sum += j++;
+        
+        if (sum == n)
+            answer++;
+    }
+    return answer;
+}
