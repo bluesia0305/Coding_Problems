b3603a3b4776f3555082d9651313f30ebb1e8338
ssumdaeng_e <79248724+bluesia0305@users.noreply.github.com>
2023-08-24 21:28:37 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 3.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int solution(int n)
+{
+	if (n > 1)
+	{
+		if (n % 2)
+			return solution(n / 2) + 1;
+	
+		return solution(n / 2);
+	}
+	
+	return 1;
+}
