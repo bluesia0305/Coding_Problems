f221684ed4dcfa4bc1aff681945e842fc4752e11
ssumdaeng_e <79248724+bluesia0305@users.noreply.github.com>
2023-12-05 14:44:01 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int brown, int yellow)
+{
+    vector<int> answer(2);
+
+    int innerRow = 1;    
+    while (true)
+    {
+        int innerCol = yellow / innerRow;
+        if (innerRow * innerCol == yellow)
+        {
+            if (2 * innerRow + 2 * innerCol + 4 == brown)
+            {
+                answer[0] = innerCol + 2;
+                answer[1] = innerRow + 2;
+                break;
+            }
+        }
+
+        innerRow++;
+        continue;
+    }
+    
+    return answer;
+}
