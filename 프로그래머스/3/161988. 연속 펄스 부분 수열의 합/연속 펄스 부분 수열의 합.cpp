30f135233fa3287ce118a6fab6a634b2d59f1545
ssumdaeng_e <79248724+bluesia0305@users.noreply.github.com>
2023-12-14 01:32:12 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 6.95 ms, Memory: 22.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+
+using namespace std;
+
+long long pulsify(vector<int> sequence, int sgn)
+{
+	long long maxSum = 0;
+	long long sum = 0;
+	int idx = 0;
+
+	while (idx < (int)sequence.size())
+	{
+		int num = sgn * sequence[idx];
+
+		if (sum + num > 0)
+		{
+			sum += num;
+			if (maxSum < sum)
+				maxSum = sum;
+		}
+		else
+			sum = 0;
+
+		sgn *= -1;
+		idx++;
+	}
+
+	return maxSum;
+}
+
+long long solution(vector<int> sequence)
+{
+	long long num1 = pulsify(sequence, 1);
+	long long num2 = pulsify(sequence, -1);
+
+	return num1 > num2 ? num1 : num2;
+}
