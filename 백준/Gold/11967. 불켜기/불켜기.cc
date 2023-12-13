2ba1efe4d717b225aaeaf74f308f023cd28fe7fd
ssumdaeng_e <79248724+bluesia0305@users.noreply.github.com>
2023-12-14 00:44:53 +0900

    [Gold II] Title: 불켜기, Time: 8 ms, Memory: 2816 KB -BaekjoonHub

--- 백준/Gold/11967. 불켜기/불켜기.cc
+++ 백준/Gold/11967. 불켜기/불켜기.cc
@@ -1,74 +1,54 @@
 #include <iostream>
-#include <map>
 #include <vector>
 #include <queue>
-
+#define MAX 102
 using namespace std;
 
-#define Coord pair<int, int>
-#define X first
-#define Y second
+int dx[] = { -1,1,0,0 };
+int dy[] = { 0,0,-1,1 };
 
-int Map[102][102];		// 0: Switch Off, 1: Switch On, 2: Connected to (1, 1)
-map<Coord, vector<Coord>> Switches;
+int n, m;
+bool map[MAX][MAX], visit[MAX][MAX];
+vector<pair<int, int>> sw[MAX][MAX];
 
-int dx[4] = { -1, 1,  0, 0 };
-int dy[4] = {  0, 0, -1, 1 };
-
-int main()
-{
+int main() {
 	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
+	cin.tie(NULL); cout.tie(NULL);
 
-	int N, M;
-	cin >> N >> M;
-	while (M--)
-	{
+	cin >> n >> m;
+	for (int i = 0; i < m; i++) {
 		int x, y, a, b;
 		cin >> x >> y >> a >> b;
-		Switches[Coord{ x, y }].push_back(Coord{ a, b });
+		sw[x][y].push_back({ a, b });
 	}
 
-	queue<Coord> q;
-	q.push(Coord{ 1, 1 });
-	Map[1][1] = 2;
-	int lights = 1;
-	while (!q.empty())
-	{
-		Coord curr = q.front();
+	int cnt = 1;
+	queue<pair<int, int>> q;
+	q.push({ 1, 1 });
+	map[1][1] = visit[1][1] = true;
+	while (!q.empty()) {
+		int x = q.front().first;
+		int y = q.front().second;
 		q.pop();
 
-		for (Coord c : Switches[curr])
-			if (!Map[c.X][c.Y])
-			{
-				Map[c.X][c.Y] = 1;
-				lights++;
-				for (int i = 0; i < 4; i++)		// whenever switch turn on, find adjacent visited room
-				{
-					int x = c.X + dx[i];
-					int y = c.Y + dy[i];
-					if (Map[x][y] == 2)
-					{
-						Map[c.X][c.Y] = 2;
-						q.push(c);
-						break;
-					}
-				}
+		for (auto& p : sw[x][y]) {
+			if (map[p.first][p.second]) continue;
+			map[p.first][p.second] = true;
+			for (int i = 0; i < 4; i++) {
+				int nx = p.first + dx[i];
+				int ny = p.second + dy[i];
+				if (visit[nx][ny]) q.push({ nx, ny });
 			}
+			cnt++;
+		}
 
-		for (int i = 0; i < 4; i++)
-		{
-			int x = curr.X + dx[i];
-			int y = curr.Y + dy[i];
-			if (Map[x][y] == 1)
-			{
-				Map[x][y] = 2;
-				q.push(Coord{ x, y });
-			}
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if (visit[nx][ny] || !map[nx][ny]) continue;
+			q.push({ nx, ny });
+			visit[nx][ny] = true;
 		}
 	}
-
-	cout << lights << "\n";
-	return 0;
+	cout << cnt;
 }
