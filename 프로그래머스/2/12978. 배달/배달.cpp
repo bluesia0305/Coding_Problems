cbfe0cc089ad5d0db5b4a7120d1f347006b2af21
ssumdaeng_e <79248724+bluesia0305@users.noreply.github.com>
2023-12-21 18:50:53 +0900

    [level 2] Title: 배달, Time: 0.10 ms, Memory: 4.12 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.cpp
+++ 프로그래머스/2/12978. 배달/배달.cpp
@@ -2,36 +2,11 @@
 #include <vector>
 using namespace std;
 
-int Towns[51][51];
-
-int solution(int N, vector<vector<int>> road, int K)
-{
+int solution(int N, vector<vector<int> > road, int K) {
     int answer = 0;
 
-    for (int i = 1; i <= N; i++)
-        for (int j = 1; j <= N; j++)
-            if (i != j)
-                Towns[i][j] = 99999999;
-    
-    for (vector<int> r : road)
-        if (Towns[r[0]][r[1]] > r[2])
-        {
-            Towns[r[0]][r[1]] = r[2];    
-            Towns[r[1]][r[0]] = r[2];
-        }
-    
-    for (int w = 1; w <= N; w++)
-        for (int i = 1; i <= N; i++)
-            for (int j = 1; j <= N; j++)
-            {
-                int dist = Towns[i][w] + Towns[w][j];
-                if (Towns[i][j] > dist)
-                    Towns[i][j] = dist;
-            }
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    cout << "Hello Cpp" << endl;
 
-    for (int i = 1; i <= N; i++)
-        if (Towns[1][i] <= K)
-            answer++;
-    
     return answer;
 }
