c84d7c129cd42a2522fb7b45a1ed59b99be5da61
ssumdaeng_e <79248724+bluesia0305@users.noreply.github.com>
2023-12-22 17:59:11 +0900

    [Silver V] Title: 영화감독 숌, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -5,20 +5,52 @@ using namespace std;
 
 int main()
 {
-	int N, Ans = 665;
+	int N;
 	cin >> N;
 
-	for (int cnt = 1; cnt <= N; cnt++)
+	int n = 0;
+	int cnt = 0;
+	while (true)
 	{
+		int left = n;
+		int sixes = 0;
+		int tens = 1;
 		while (true)
 		{
-			Ans++;
-
-			if (to_string(Ans).find("666", 0, 3) != string::npos)
+			if (left % 10 == 6)
+			{
+				sixes++;
+				tens *= 10;
+				left /= 10;
+			}
+			else
 				break;
 		}
-	}
 
-	cout << Ans << "\n";
-	return 0;
+		if (sixes)
+		{
+			for (int i = 0; i < tens; i++)
+			{
+				string right = to_string(i);
+				while (right.size() < sixes)
+					right = "0" + right;
+
+				if (++cnt == N)
+				{
+					cout << stoi(to_string(left) + "666" + right) << "\n";
+					return 0;
+				}
+			}
+		}
+		else
+		{
+			if (++cnt == N)
+			{
+				cout << stoi(to_string(n) + "666") << "\n";
+				return 0;
+			}
+		}
+        
+		n++;
+	}
 }
