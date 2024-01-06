49fac9b3156564a272583c3dc665f1bb9b16803b
ssumdaeng_e <79248724+bluesia0305@users.noreply.github.com>
2024-01-07 01:55:05 +0900

    [level 2] Title: 타겟 넘버, Time: 0.04 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -1,25 +1,24 @@
-#include <string>
 #include <vector>
 
 using namespace std;
 
-void Add(const vector<int>& v, int size, int& cnt, int i, int t, int sum)
+void Add(const vector<int>& v, const int& t, int& cnt, int i, int sum)
 {
-    if (i == size)
+    if (i == v.size())
     {
         if (t == sum)
             cnt++;
         return;
     }
     
-    Add(v, size, cnt, i + 1, t, sum + v[i]);
-    Add(v, size, cnt, i + 1, t, sum - v[i]);
+    Add(v, t, cnt, i + 1, sum + v[i]);
+    Add(v, t, cnt, i + 1, sum - v[i]);
 }
 
 int solution(vector<int> numbers, int target)
 {
     int answer = 0;
-    Add(numbers, numbers.size(), answer, 1, target, numbers[0]);
-    Add(numbers, numbers.size(), answer, 1, target, -numbers[0]);
+    Add(numbers, target, answer, 1,  numbers[0]);
+    Add(numbers, target, answer, 1, -numbers[0]);
     return answer;
 }
