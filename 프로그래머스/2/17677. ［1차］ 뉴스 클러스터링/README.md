02fa5766216ea6768828047f9323beec189e2d2e
ssumdaeng_e <79248724+bluesia0305@users.noreply.github.com>
2024-01-06 21:39:42 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/README.md
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 뉴스 클러스터링 - 17677 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17677) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17677#qna) 
 
 ### 성능 요약
 
-메모리: 4.17 MB, 시간: 0.04 ms
+메모리: 4.13 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 6일 21:7:49
+2024년 1월 6일 21:39:39
 
 ### 문제 설명
 
--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.cpp
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.cpp
@@ -1,51 +1,27 @@
 #include <string>
-#include <unordered_map>
-#include <unordered_set>
 #include <algorithm>
 
 using namespace std;
 
-bool ischar(char c)
-{
-    if ('a' <= c && c <= 'z')
-        return true;
-    
-    return false;
-}
+int pairs1[676];
+int pairs2[676];
 
 int solution(string str1, string str2)
 {
-    for (char& c : str1)
-        c = tolower(c);
-    
-    for (char& c : str2)
-        c = tolower(c);
-    
-    unordered_map<string, int> pairs1, pairs2;
-    unordered_set<string> pairs;
     for (int i = 0; i < str1.size() - 1; i++)
-        if (ischar(str1[i]) && ischar(str1[i + 1]))
-        {
-            pairs1[str1.substr(i, 2)]++;
-            pairs.emplace(str1.substr(i, 2));
-        }
-    
+        if (isalpha(str1[i]) && isalpha(str1[i + 1]))
+            pairs1[(str1[i] & 31) * 26 + (str1[i + 1] & 31)]++;
+        
     for (int i = 0; i < str2.size() - 1; i++)
-        if (ischar(str2[i]) && ischar(str2[i + 1]))
-        {
-            pairs2[str2.substr(i, 2)]++;
-            pairs.emplace(str2.substr(i, 2));
-        }
+        if (isalpha(str2[i]) && isalpha(str2[i + 1]))
+            pairs2[(str2[i] & 31) * 26 + (str2[i + 1] & 31)]++;
     
     int a = 0, b = 0;
-    for (auto& s : pairs)
+    for (int i = 0; i < 676; i++)
     {
-        a += min(pairs1[s], pairs2[s]);
-        b += max(pairs1[s], pairs2[s]);
+        a += min(pairs1[i], pairs2[i]);
+        b += max(pairs1[i], pairs2[i]);
     }
-
-    if (b)
-        return int(float(a) / b * 65536);
-    else
-        return 65536;
+    
+    return b ? a * 65536 / b : 65536;
 }
