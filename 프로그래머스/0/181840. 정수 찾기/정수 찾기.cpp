2aa161a2b85b8c285293a7129d89467351ac708e
ssumdaeng_e <79248724+bluesia0305@users.noreply.github.com>
2024-10-24 06:57:18 +0900

    [level 0] Title: 정수 찾기, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.cpp
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.cpp
@@ -3,11 +3,7 @@
 
 using namespace std;
 
-int solution(vector<int> num_list, int n)
-{
-    for (const int& num : num_list)
-        if (num == n)
-            return 1;
-    
-    return 0;
+int solution(vector<int> num_list, int n) {
+    int answer = 0;
+    return answer;
 }
