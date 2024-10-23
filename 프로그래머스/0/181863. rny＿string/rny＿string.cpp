fce962818101e730e622dc3aee57089a8e76c882
ssumdaeng_e <79248724+bluesia0305@users.noreply.github.com>
2024-10-24 07:47:15 +0900

    [level 0] Title: rny_string, Time: 0.02 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/0/181863. rny＿string/rny＿string.cpp
+++ 프로그래머스/0/181863. rny＿string/rny＿string.cpp
@@ -1,12 +1,11 @@
 #include <string>
-#include <vector>
 
 using namespace std;
 
 string solution(string rny_string)
 {
     auto i = rny_string.find("m");
-    while (i < rny_string.size())
+    while (i != string::npos)
     {
         rny_string.replace(i, 1, "rn");
         i = rny_string.find("m");
