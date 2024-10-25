96f1f9c1dec97f2c31982ddac45add47eda52fe4
ssumdaeng_e <79248724+bluesia0305@users.noreply.github.com>
2024-10-26 03:28:31 +0900

    [level 2] Title: 모음 사전, Time: 0.62 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+
+#include <iostream>
+
+using namespace std;
+
+string Word[5] = { "A", "E", "I", "O", "U" };
+
+vector<string> Dictionary;
+
+void MakeDictionary(string str, string find_word, bool& bFind)
+{   
+    if (str.size() > 5 || bFind)
+        return;
+    
+    if (str == find_word)
+        bFind = true;
+    
+    if (str.size() > 0)
+        Dictionary.push_back(str);
+        
+    for (int i = 0; i < 5; i++)
+        MakeDictionary(str + Word[i], find_word, bFind);
+}
+
+int solution(string word)
+{
+    bool bIsFinded = false;
+    MakeDictionary("", word, bIsFinded);    
+    return Dictionary.size();
+}
