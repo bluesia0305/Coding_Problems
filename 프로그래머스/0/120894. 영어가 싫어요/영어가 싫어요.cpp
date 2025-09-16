a08e1d2f8006f38c5ea785e2fd3db2c7424b6d50
ssumdaeng_e <79248724+bluesia0305@users.noreply.github.com>
2025-09-16 22:52:38 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.03 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.cpp
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.cpp
@@ -5,16 +5,14 @@ using namespace std;
 
 long long solution(string numbers)
 {
-    string eng[] = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
-    string num[] = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-    
-    for (int i = 0; i < 10; i++)
+    vector<string> words = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+    for (int i = 0; i < words.size(); i++)
     {
-        auto idx = numbers.find(eng[i]);
+        auto idx = numbers.find(words[i]);
         while (idx != std::string::npos)
         {
-            numbers.replace(idx, eng[i].size(), num[i]);
-            idx = numbers.find(eng[i]);
+            numbers.replace(idx, words[i].size(), to_string(i));
+            idx = numbers.find(words[i]);
         }
     }
     
