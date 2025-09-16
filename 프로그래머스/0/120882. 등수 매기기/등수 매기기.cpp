9143c7fd7bc1bb94f53b70f858929e42dafcebf4
ssumdaeng_e <79248724+bluesia0305@users.noreply.github.com>
2025-09-16 13:49:08 +0900

    [level 0] Title: 등수 매기기, Time: 0.02 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.cpp
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.cpp
@@ -1,32 +1,21 @@
 #include <string>
 #include <vector>
-#include <map>
-#include <algorithm>
 
 using namespace std;
 
 vector<int> solution(vector<vector<int>> score)
 {
-    map<vector<int>, int> map;
-    vector<vector<int>> sorted = score;
-    sort(sorted.begin(), sorted.end(),
-         [](const vector<int>& a, const vector<int>& b)
-         {
-             return a[0] + a[1] > b[0] + b[1];
-         });
-    
-    for (int i = 0; i < sorted.size(); i++)
-    {
-        map[sorted[i]] = i + 1;
-        
-        if (i > 0)
-            if (sorted[i][0] + sorted[i][1] == sorted[i - 1][0] + sorted[i - 1][1])
-                map[sorted[i]] = map[sorted[i - 1]];
-    }
-    
     vector<int> answer(score.size());
-    for (int i = 0; i < score.size(); i++)
-        answer[i] = map[score[i]];
-        
+    for (vector<int>& v : score)
+        v.push_back(v[0] + v[1]);
+    
+    for (vector<int> v : score)
+        for (int i = 0; i < score.size(); i++)
+            if (v[2] > score[i][2])
+                answer[i]++;
+    
+    for (int& a : answer)
+        a++;
+    
     return answer;
 }
