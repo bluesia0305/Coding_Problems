b6a05ed72c8a46e36550845d36ef853d2fbc91db
ssumdaeng_e <79248724+bluesia0305@users.noreply.github.com>
2025-09-16 22:16:59 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.cpp
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.cpp
@@ -5,9 +5,5 @@ using namespace std;
 
 int solution(vector<int> sides)
 {
-    int sum = sides[0] + sides[1];
-    int longer = sides[0] > sides[1] ? sides[0] : sides[1];
-    int shorter = sides[0] > sides[1] ? sides[1] : sides[0];
-
-    return sum - 1 - longer + shorter;
+    return sides[0] < sides[1] ? 2 * sides[0] - 1 : 2 * sides[1] - 1;
 }
