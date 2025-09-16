280eeeb33b9a24fe291b27236710026c8b59c09a
ssumdaeng_e <79248724+bluesia0305@users.noreply.github.com>
2025-09-16 23:23:32 +0900

    [level 0] Title: 배열 만들기 4, Time: 1.63 ms, Memory: 6.42 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr)
+{
+    vector<int> stk;
+    for (int i = 0; i < arr.size();)
+    {
+        if (stk.empty())
+            stk.push_back(arr[i++]);
+        else
+        {
+            if (stk.back() < arr[i])
+                stk.push_back(arr[i++]);
+            else
+                stk.pop_back();
+        }
+    }
+    return stk;
+}
