6691ff15c50ed23d1644482bc711640b1e6554f2
ssumdaeng_e <79248724+bluesia0305@users.noreply.github.com>
2025-09-16 14:35:35 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.03 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.cpp
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.cpp
@@ -6,26 +6,15 @@ using namespace std;
 
 int solution(vector<int> rank, vector<bool> attendance)
 {
-    int answer = 0;
-    int coef = 10000;
-    int r = 1;
-    int count = 0;
-    while (count < 3)
-    {
-        int idx = 0;
-        for (; idx < rank.size(); idx++)
-            if (rank[idx] == r)
-                break;
-        
-        if (attendance[idx])
-        {
-            answer += idx * coef;
-            count++;
-            coef /= 100;
-        }
-            
-        r++;
-    }
-
-    return answer;
+    vector<vector<int>> students;
+    for (int i = 0; i < rank.size(); i++)
+        if (attendance[i]) students.push_back(vector<int>{ i, rank[i] });
+    
+    sort(students.begin(), students.end(),
+         [](const vector<int>& a, const vector<int>& b)
+         {
+             return a[1] < b[1];
+         });
+    
+    return students[0][0] * 10000 + students[1][0] * 100 + students[2][0];
 }
