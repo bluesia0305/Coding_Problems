813d320dc28a0f31e3f1b18c145d6f5c5d5c7e41
ssumdaeng_e <79248724+bluesia0305@users.noreply.github.com>
2025-09-21 13:43:25 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.cpp
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.cpp
@@ -6,11 +6,13 @@ using namespace std;
 
 int solution(vector<int> num_list)
 {
-    int sumOdd = 0, sumEven = 0;
-    int answer = 0;
-    for (int i = 0; i < num_list.size(); i += 2)
-        sumOdd += num_list[i];
-    for (int i = 1; i < num_list.size(); i += 2)
+    int sumOdd = 0, sumEven = 0, n = num_list.size();
+    
+    for (int i = 0; i < n; i += 2)
+        sumOdd  += num_list[i];
+    
+    for (int i = 1; i < n; i += 2)
         sumEven += num_list[i];
+
     return max(sumOdd, sumEven);
 }
