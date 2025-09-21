91b7554aea394010a12fa95054fc054ebc413a63
ssumdaeng_e <79248724+bluesia0305@users.noreply.github.com>
2025-09-21 13:23:41 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> str_list, string ex)
+{
+    string answer = "";
+    for (string s : str_list)
+        if (s.find(ex) == string::npos)
+            answer += s;
+    return answer;
+}
